18eda3547d13d5eb5cc7f6275ca35b6564484c06
Morgan Prior <morgan@amaras-tech.co.uk>
2012-04-25 23:49:23 +0100

    File has main contents

--- lib/maruku-snippet/maruku-snippet.rb
+++ lib/maruku-snippet/maruku-snippet.rb
@@ -1,6 +1,69 @@
+module MaRuKuSnippet
+class Document
 
-module MarukuSnippet
-class X
+    def initialize( markdown )
+      @markdown = markdown
+    end
+
+  ## TODO or maybe not, we currently cut styling  -- == underlining if it is on the line after the cut off length.
+  # would you want a snippet ending with a heading though?
+
+  def to_html_snippet( lines=-1 )
+    if lines == -1
+      markdown_temp = @markdown
+    else
+      markdown_a    = @markdown.split("\n")
+      markdown_temp = (markdown_a[0...lines]).join("\n")
+
+      ## Is the input text longer than cut off length
+      if markdown_a.size > lines
+        ## Are we mid table ?
+        mid_table       = false
+        if markdown_a[lines-1].match(/\|/) and markdown_a[lines].match(/\|/)
+          mid_table = true
+        end
+
+        ## Looking for markdown elements we can not break (Link targets, tables)
+        (markdown_a[lines...(markdown_a.size)]).each_with_index do |text, index| 
+          ## Make sure keep link targets
+          if text.match(/^[ \t ]*\[[\W\w]*\]:[\W\w]*/)
+            markdown_temp << "\n" + text
+          end
+
+          if mid_table
+            #Still parsing table or have we finished
+            if text.match(/\|/) 
+              markdown_temp << "\n" + text
+            else
+              mid_table        = false
+            end
+          end
+
+        end
+      end
+    end
+
+    begin
+      html_string = Maruku.new(markdown_temp).to_html   
+      html_string.gsub!("<pre>","<pre class=\"prettyprint\">")
+    rescue
+      html_string = 'Post rendering failed, possibly due to an illegal charater'
+    end
+    return html_string
+  end
+
+  ## Delegate to to Maruku via method missing
+  def method_missing(method, *args, &block)
+    puts method
+    delegate = Maruku.new( @markdown )
+    if delegate.respond_to? method
+        return delegate.send(method)
+      else
+         raise NoMethodError
+      end
+  end
 end
+
 end
+
 

54e51e99eeaeb5963c8f4aa7a608f8c24043052a
Morgan Prior <morgan@amaras-tech.co.uk>
2012-04-26 00:11:53 +0100

    require relative for 1.9.2 compatability

--- lib/maruku_snippet.rb
+++ lib/maruku_snippet.rb
@@ -5,7 +5,12 @@ module MaRuKuSnippet
   VERSION = '0.0.1'
 end
 
-require 'maruku_snippet/maruku_snippet_document'
+## ruby 1.9.2+ methods first fall back to 1.8 style 
+begin
+  require_relative 'maruku_snippet/maruku_snippet_document'
+rescue
+  require 'maruku_snippet/maruku_snippet_document'
+end
 
 # This is the public interface
 class MarukuSnippet < MaRuKuSnippet::Document; 
